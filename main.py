ae11bcd6aa4382ad916ca304b0cc244065b9e26a
sanjanaj <sanjanaj@umich.edu>
2023-06-23 23:08:00 -0700

    assignment 2 code

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 def main():
     """
     ##################################################
-    Comlete your code here
+    Complete your code here
     Use m_perc and f_perc for your results
     ##################################################
     """
+
+    male = int(input('Number males: '))
+    female = int(input('Number female: '))
+
+    total = male + female
+
+    m_perc = (male / total) * 100
+    f_perc = (female / total) * 100
+
+    print("The total number of students: " + str(total))
+    print("The number of males and females: " + str(male) + " " + str(female))
+    print(f"The percentage of males and females: {m_perc:.2f} \t {f_perc:.2f}")
 
     """
     ########################################
